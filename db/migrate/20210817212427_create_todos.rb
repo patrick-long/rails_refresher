37b29ec8c5e43da75076e85eef8d49babb0e0dc5
longpatrick <long.patrick1997@gmail.com>
2021-08-17 16:26:23 -0500

    add more columns for migration to add

--- db/migrate/20210817212427_create_todos.rb
+++ db/migrate/20210817212427_create_todos.rb
@@ -1,7 +1,8 @@
 class CreateTodos < ActiveRecord::Migration[6.1]
   def change
     create_table :todos do |t|
-
+      t.string :title
+      t.boolean :completed
       t.timestamps
     end
   end
